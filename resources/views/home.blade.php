6b91a1aea9306b3581f8eee7e8a8bd764727dd82
Rangers69 <ahmadwidodo89@gmail.com>
2022-08-23 23:53:15 +0700

    setup view home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,24 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-lg-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                <div class="card-px text-center py-20 my-10">
+                    <!--begin::Title-->
+                    <h2 class="fs-2x fw-bolder mb-10">Welcome to Karomah Learning Center</h2>
+                    <!--end::Title-->
+                    <!--begin::Description-->
+                    <p class="text-gray-400 fs-4 fw-bold mb-10">Lakukan yang terbaik
+                        <br>di semua kesempatan yang kamu miliki.
+                    </p>
+                    <!--end::Description-->
+                    <!--begin::Action-->
+                    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_add_customer">keep up the good work</a>
+                    <!--end::Action-->
                 </div>
             </div>
+
         </div>
     </div>
 </div>
