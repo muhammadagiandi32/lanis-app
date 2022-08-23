f259a2380b32c35187669ba7f20d9acc964d9ac9
Rangers69 <ahmadwidodo89@gmail.com>
2022-08-23 23:36:13 +0700

    setup middleware

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('ketua', function (User $user) {
+            return $user->role_id == 1;
+        });
+
+        Gate::define('admin', function (User $user) {
+            return $user->role_id == 2;
+        });
+
+        Gate::define('siswa', function (User $user) {
+            return $user->role_id == 3;
+        });
     }
 }
